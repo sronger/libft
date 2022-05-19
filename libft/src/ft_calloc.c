df7c6d4a44f8c886f1a40c0ce3ab008358c59a34
Juntae <79131091+sronger@users.noreply.github.com>
2022-05-19 16:47:55 +0900

    Add files via upload

--- libft/src/ft_calloc.c
+++ libft/src/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: juntakim <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 16:16:02 by juntakim          #+#    #+#             */
-/*   Updated: 2022/05/17 17:59:58 by juntakim         ###   ########.fr       */
+/*   Updated: 2022/05/17 22:04:54 by juntakim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,6 @@ void	*ft_calloc(size_t count, size_t size)
 	arr = malloc(size * count);
 	if (!arr)
 		return (NULL);
-	ft_memset(arr, 0, size * count)
+	ft_memset(arr, 0, size * count);
 	return (arr);
 }
